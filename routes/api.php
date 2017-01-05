e11e603ad61679368baea0afc47a1d045fa17588
Markus Koehler <info@koehler.pro>
2017-01-05 13:02:14 +0100

    rename timediff route, disable password reset for now

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,28 @@ use Dingo\Api\Routing\Router;
 $api = app(Router::class);
 
 $api->version('v1', function (Router $api) {
+    // auth routes
     $api->group(['prefix' => 'auth'], function(Router $api) {
-        $api->post('signup', 'App\\Api\\V1\\Controllers\\SignUpController@signUp');
-        $api->post('login', 'App\\Api\\V1\\Controllers\\LoginController@login');
+        $api->post('signup', 'App\Api\V1\Controllers\SignUpController@signUp');
+        $api->post('login', 'App\Api\V1\Controllers\LoginController@login');
 
-        $api->post('recovery', 'App\\Api\\V1\\Controllers\\ForgotPasswordController@sendResetEmail');
-        $api->post('reset', 'App\\Api\\V1\\Controllers\\ResetPasswordController@resetPassword');
+        // todo not implemented yet
+        //$api->post('recovery', 'App\Api\V1\Controllers\ForgotPasswordController@sendResetEmail');
+        //$api->post('reset', 'App\Api\V1\Controllers\ResetPasswordController@resetPassword');
     });
 
     $api->group(['middleware' => 'jwt.auth'], function(Router $api) {
-        $api->get('protected', function() {
+        $api->get('timediff', function() {
+            $diff = (new \DateTime('2013-10-09 00:00:00'))->diff(new \DateTime());
+            //$diff = (new \DateTime('@0'))->diff(new \DateTime('@102211200'));
             return response()->json([
-                'message' => 'Access to this item is only for authenticated user. Provide a token in your request!'
+                //'message' => 'Access to this item is only for authenticated user. Provide a token in your request!'
+                /*"years" => (new \DateTime('@0'))->diff(new \DateTime('@102211200'))->y,
+                "months" => (new \DateTime('@0'))->diff(new \DateTime('@102211200'))->m,
+                "days" => (new \DateTime('@0'))->diff(new \DateTime('@102211200'))->d*/
+                "years" => $diff->y,
+                "months" => $diff->m,
+                "days" => $diff->d
             ]);
         });
 
